82dd7418fa5c5357315433268b8bc228fe04eb26
Danesh <techydanesh@gmail.com>
2026-06-29 18:13:23 +0530

    test: home page clerk auth and org operation test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { toast } from "sonner";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <Button
-      onClick={() => {
-        toast.success("hellow world");
-      }}
-    >
-      Click me
-    </Button>
+    <div className='flex min-h-screen flex-col items-center justify-center'>
+      <h1
+        className='flex items-center
+      gap-4'
+      >
+        Welcome to Roons
+      </h1>
+      <OrganizationSwitcher />
+      <UserButton />
+    </div>
   );
 }
