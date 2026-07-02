68d03fa3faa12257a95098e81246880dc840b303
Danesh <techydanesh@gmail.com>
2026-07-02 18:17:56 +0530

    init: tRPC initialization setup and health check

---
+++ src/trpc/routers/_app.ts
@@ -0,0 +1,8 @@
+import { baseProcedure, createTRPCRouter } from "../init";
+export const appRouter = createTRPCRouter({
+  health: baseProcedure.query(async () => {
+    return { status: "ok", code: 200 };
+  }),
+});
+// export type definition of API
+export type AppRouter = typeof appRouter;
