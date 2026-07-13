3e9ae8b49630b9bedf03d8441cb1b68144a78fd0
Danesh <techydanesh@gmail.com>
2026-07-13 16:20:29 +0530

    feat: trpc voice api creation

---
+++ src/trpc/routers/voices.ts
@@ -0,0 +1,108 @@
+import z from "zod";
+import { prisma } from "@/lib/database";
+import { createTRPCRouter, orgProcedure } from "../init.js";
+import { TRPCError } from "@trpc/server";
+import { deleteAudio } from "@/lib/r2.js";
+
+export const voicesRouter = createTRPCRouter({
+  getAll: orgProcedure
+    .input(
+      z
+        .object({
+          query: z.string(),
+        })
+        .optional(),
+    )
+    .query(async ({ ctx, input }) => {
+      const searchFilter = input?.query
+        ? {
+            OR: [
+              {
+                name: {
+                  contains: input.query,
+                  mode: "insensitive" as const,
+                },
+              },
+              {
+                description: {
+                  contains: input.query,
+                  mode: "insensitive" as const,
+                },
+              },
+            ],
+          }
+        : {};
+
+      const [custom, system] = await Promise.all([
+        prisma.voice.findMany({
+          where: {
+            variant: "CUSTOM",
+            orgId: ctx.orgId,
+            ...searchFilter,
+          },
+          orderBy: { createdAt: "desc" },
+          select: {
+            id: true,
+            name: true,
+            description: true,
+            createdAt: true,
+            updatedAt: true,
+            variant: true,
+          },
+        }),
+        prisma.voice.findMany({
+          where: {
+            variant: "SYSTEM",
+            ...searchFilter,
+          },
+          orderBy: { name: "asc" },
+          select: {
+            id: true,
+            name: true,
+            description: true,
+            category: true,
+            language: true,
+            variant: true,
+          },
+        }),
+      ]);
+      return [...system, ...custom];
+    }),
+  delete: orgProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const voice = await prisma.voice.findUnique({
+        where: {
+          id: input.id,
+          variant: "CUSTOM",
+          orgId: ctx.orgId,
+        },
+        select: {
+          id: true,
+          dataObjectKey: true,
+        },
+      });
+
+      if (!voice) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Voice not found",
+        });
+      }
+
+      await prisma.voice.delete({
+        where: {
+          id: voice.id,
+        },
+      });
+
+      if (voice.dataObjectKey) {
+        // in production we will consider background jobs for this, but for now we will just delete it and log any errors
+        await deleteAudio(voice.dataObjectKey).catch((err) => {
+          console.error("Failed to delete audio from R2:", err);
+        });
+      }
+
+      return { success: true };
+    }),
+});
